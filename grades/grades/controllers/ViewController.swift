752ed6b4c2e4aa6395bfb61b252b6345554f1c8e
CristianAlexanderArboledaMuñoz <lk-ang@hotmail.com>
2016-02-19 21:57:01 -0500

    no message

--- grades/grades/controllers/ViewController.swift
+++ grades/grades/controllers/ViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var courses = [Course]()
+    let cellIdenti
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        courses = Course.courses()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, nunberOfRowsInsection section: Int)->Int {
+        return courses.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        NSIndexPathˇ
     }
 
 
--- grades/grades/models/Course.swift
+++ grades/grades/models/Course.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+class Course {
+    var name: String?
+    var grade: Float?
+    
+    
+    init(name: String, grade: Float){
+    
+        self.name = name
+        self.grade = grade
+    
+    
+    }
+    
+    convenience init(){
+    
+        self.init(name:"", grade:0.0)
+    }
+    
+    
+    //MARK: Utils
+    class func courses() ->[Course]{
+        var data = [Course]()
+        let rawData = {
+        
+        ["name":"Formulacion y evaluacion de proyectos", "grade":3.5],
+        ["name":"Ing Soft III", "grade":3.5],
+        ["name":"Sistemas de informacion", "grade":4.5],
+        ["name":"Sistemas Operativos ","grade":2.5],
+        ["name":"Algebra", "grade":3.5],
+        ["name":"analisis", "grade":4.5]
+            
+        }
+        
+        for item in rawData{
+            let course = Course(name: item["name"] as! String, grade: item["grade"] as! Float)
+            )
+            data.append(course)
+        }
+        return data
+    }
+}
